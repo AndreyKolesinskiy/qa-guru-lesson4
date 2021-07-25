9a9a8d57491d011208a2f9ab8f68f414e04c482c
Andrei Kaliasinski <andykolesinski@gmail.com>
2021-07-25 16:24:24 +0300

    Added Github test

--- src/test/java/tests/GithubTest.java
+++ src/test/java/tests/GithubTest.java
@@ -1,13 +1,22 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 
 public class GithubTest extends BaseTest {
 
     @Test
-    public void checkJUnit5SampleCode(){
-        Selenide.open("https://github.com/");
-
+    public void checkJUnit5SampleCode() {
+        Selenide.open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").$(byText("Soft assertions")).shouldBe(visible).click();
+        //*[text()='Or register extension inside test class:']//following-sibling::div
+        $(byText("Or register extension inside test class:")).sibling(0)
+                .shouldHave(Condition.cssClass("highlight-source-java")).shouldBe(visible);
     }
 }
