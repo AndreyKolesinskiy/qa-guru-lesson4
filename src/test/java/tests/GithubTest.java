0f3d9619fdde8df65656ecf2ed3c093263f1cbe7
Andrei Kaliasinski <andykolesinski@gmail.com>
2021-07-25 17:47:19 +0300

    Updated tests

--- src/test/java/tests/GithubTest.java
+++ src/test/java/tests/GithubTest.java
@@ -1,21 +1,20 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class GithubTest extends BaseTest {
 
     @Test
     public void checkJUnit5SampleCode() {
-        Selenide.open("https://github.com/selenide/selenide");
+        open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-        $("#wiki-body").$(byText("Soft assertions")).shouldBe(visible).click();
-        $(byText("Or register extension inside test class:")).sibling(0)
-                .shouldHave(Condition.cssClass("highlight-source-java")).shouldBe(visible);
+        $("#wiki-pages-box").$(".js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(byText("Using JUnit5 extend test class:")).shouldBe(visible);
     }
 }
