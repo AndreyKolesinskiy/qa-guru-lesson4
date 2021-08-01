6549288d3bd58e2a1c73c22ccc4232d1ac6bed7f
Andrei Kaliasinski <andykolesinski@gmail.com>
2021-08-01 22:53:26 +0300

    All three commits together

--- src/test/java/tests/GithubTest.java
+++ src/test/java/tests/GithubTest.java
@@ -16,5 +16,8 @@ public class GithubTest extends BaseTest {
         $("#wiki-pages-box").$(".js-wiki-more-pages-link").click();
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
         $(byText("Using JUnit5 extend test class:")).shouldBe(visible);
+        //Fist test commit
+        //Second test commit
+        //Third test commit
     }
 }
